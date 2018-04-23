d6ff360506f1abb6be1504dd60d8257540c98cd5
Monika Radosiewicz <monika.radosiewicz@gmail.com>
2018-04-23 21:07:29 +0200

    add components

---
+++ components/Search.js
@@ -0,0 +1,48 @@
+var Search = React.createClass({
+    
+    // stan poczatkowy, searching bar jest pusty
+    getInitialState(){
+        return {
+            searchingText: ''
+        };
+    },
+    
+    // funkcja uruchamia się w momencie zmiany wartości input (atrybut pola input - onchange)
+    handleChange: function(event){
+        var searchingText = event.target.value; //wartość zdarzenia
+        this.setState({
+            searchingText: searchingText // zaktualizowanie stanu o wartość zdarzenia
+        });
+        
+         // jeżeli wpiszemy więcej niż dwa znaki wywołujemy funkcję, przekazaną do propsa, wysyłana wartość to wpisany tekst
+        if (searchingText.length > 2) {
+          this.props.onSearch(searchingText);
+        }
+    },
+
+    //nasłuchiwanie na kliknięcie enter --> ponowne uruchomienie funkcji wysyłającej zapytanie po gifa
+  handleKeyUp: function(event) {
+    if (event.keyCode === 13) {
+      this.props.onSearch(this.state.searchingText);
+    }
+  },
+    
+    render: function(){
+        
+        var styles = {
+            fontSize: '1.5em',
+            width: '90%',
+            maxWidth: '350px'
+        }
+        
+        return 
+            <input
+            type='text'
+            onchange={this.handleChange}
+            onKeyUp={this.handleKeyUp}
+            placeholder='Tutaj wpisz wyszukiwaną frazę'
+            style={styles}
+            value={this.state.searchTerm}
+            />
+         }
+})
